7ee2a4eb214e2600812c8473370cfd404111a79f
Maxim Novikov <maxnovbel@gmail.com>
2020-09-29 14:09:49 +0300

    feat: add solvation for func's

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 
 exports.min = function min (array) {
+  if(array !== undefined && array.length !== 0){
+    return Math.min(...array.map(i => i));
+  }
   return 0;
 }
 
 exports.max = function max (array) {
+  if(array !== undefined && array.length !== 0){
+    return Math.max(...array.map(i => i));
+  }
   return 0;
 }
 
 exports.avg = function avg (array) {
+  if(array !== undefined && array.length !== 0){
+    let sum = 0;
+    for( let i of array){
+      sum += i;
+    };
+    return sum / array.length;
+  }
   return 0;
 }
